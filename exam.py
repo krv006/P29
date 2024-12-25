eade2504a89710b977bf96eb0d5a92ff907694f9
krv006 <rvkamronbek@gmail.com>
2024-12-26 00:03:32 +0500

    exam

---
+++ exam.py
@@ -0,0 +1,54 @@
+
+
+# 2
+# n = input('son kriting')
+# n = n.split(',')
+# max_son = int(max(n))
+# min_son = int(min(n))
+# if max_son % 2 == 0:
+#     max_son=int(max(n))-5
+# else:
+#     max_son=int(max(n))+3
+# if min_son % 2 == 1:
+#     min_son=int(min(n))-2
+# else:
+#     min_son=int(min(n))+4
+# print('max son ',max_son,'min son',min_son)
+# 3
+# javob = 1
+# n = int(input())
+# mylist = []
+# for i in range(1, n+1):
+#     if i % 2 == 0:
+#         mylist.append(i)
+#
+# for i in mylist:
+#    javob*=mylist[i-2]*mylist[i-1]
+#    print(javob)
+
+#4
+# n=int(input())
+# for i in range(n):
+#     for j in range(n):
+#         if j==i+1:
+#             print(j)
+#     print()
+#6
+# satr=input()
+# sonlar=[]
+# harf=[]
+# belgi=[]
+# for i in satr:
+#     if i.isdigit():
+#         sonlar.append(i)
+#     if i.isalpha():
+#         harf.append(i)
+#     if not i.isalnum():
+#         belgi.append(i)
+# print(sonlar,'soni',len(sonlar))
+# print(harf,'harf',len(harf))
+# print(belgi,'belgi',len(belgi))
+
+
+
+
